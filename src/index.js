07cb4b635bad8aa9dc9d6538bd81e383e15bc8fb
William Mosquera <wjmmk3@gmail.com>
2022-08-13 21:00:32 -0500

    Implementation of OpenAPI-Swagger

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 
 const express = require("express"); 
-// Modulo que maneja las peticiones HTTP en formato JSON
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");// Modulo que maneja las peticiones HTTP en formato JSON
+const apicache = require("apicache");// Import cache module
 // Import routes from v1 folder
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const v1RecordRouter = require("./v1/routes/recordRoutes");
 const v1MemberRouter = require("./v1/routes/memberRoutes");
 
+// Import la implementacion de la documentacion de Swagger
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger.js");
+
 const app = express(); 
+const cache = apicache.middleware;// Middleware para cachear las peticiones
 const PORT = process.env.PORT || 3000; 
 
-// Add routes from v1 folder to the app
 app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(cache("1 hour"));// Cachear las peticiones
+
+// Add routes from v1 folder to the app
 app.use("/api/v1/workouts", v1WorkoutRouter);
 app.use("/api/v1/records", v1RecordRouter);
 app.use("/api/v1/members", v1MemberRouter);
 
-
 app.listen(PORT, () => { 
     console.log(`API is listening on port ${PORT}`); 
+    // Add Swagger Docs to the app
+    V1SwaggerDocs(app, PORT);
 });
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -5,6 +5,28 @@ const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     type: object
+ */
 // Rutas de la API para el controlador de workouts
 router.get("/", workoutController.getAllWorkouts);
 router.get("/:workoutId", workoutController.getOneWorkout);
